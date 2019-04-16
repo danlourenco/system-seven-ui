698667870685b739787f313b6ba108b3f9ed4232
Dan Lourenco <dan.lourenco@slalom.com>
2019-04-15 21:57:39 -0700

    add storybook

--- src/components/dialog/index.js
+++ src/components/dialog/index.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import styles from './dialog.module.scss';
-import icon from './icon.png';
-const Dialog = ({ content, showWelcome, onConfirm }) => {
+const Dialog = ({ content, isOpen, onConfirm }) => {
 
   return (
-    <dialog open={ showWelcome } className={ styles.dialog}>
+    <dialog open={ isOpen } className={ styles.dialog}>
       <div className={ styles.inner }>
         <div className={ styles.content }>
-          <img src={ icon } alt={ 'Bitmap icon of developer' } />
           <div> { content }</div>
         </div>
         <div className={ styles.actions }>
